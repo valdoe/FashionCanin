e2a5e079b7d106074cb05d665e5d6dfe6bf0cff6
valdoe <valentino.dolasevic@gmail.com>
2018-08-20 10:07:09 +0200

    Materialize
    Ajout du framework materialize (CSS)

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 
@@ -26,7 +30,7 @@
 		//Affichage des données
 		echo $data['NOM_INDIVIDU']; ?>
 		<br/> PRENOM :
-		<?php echo $data['PRENOM_INDIVIDU']; ?>
+		<?php echo $data['LOGIN_INDIVIDU']; ?>
 	</p>
 	<?php } ?>
 	<?php $resultat->closeCursor(); ?>
@@ -38,6 +42,8 @@
 		die('Erreur : ' . $e->getMessage());
 	  }
 	?>
+	<!--JavaScript Materialize-->
+     <script type="text/javascript" src="js/materialize.min.js"></script>
 </body>
 
 </html>
